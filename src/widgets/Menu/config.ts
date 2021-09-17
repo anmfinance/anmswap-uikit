8fd89bf31b4ccd73a04c40ad0a7c4e065045d376
blockchainGuru125 <kkh0102www@outlook.com>
2021-09-17 15:36:39 +1000

    first commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,93 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.pastafinance.com",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.pastafinance.com/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://pancakeswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://pancakeswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://pancakeswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://pancakeswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/pastafinance/",
+      },
+      {
+        label: "Docs",
+        href: "https://vikingswap.gitbook.io/viking-swap/",
+      },
+      {
+        label: "Blog",
+        href: "https://viking-swap.medium.com/",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "CN",
+        href: "https://t.me/ANMchinafinance",
+      },
+      {
+        label: "English",
+        href: "https://t.me/ANMinternationalfinance",
+      }
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/AnMfinance",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
